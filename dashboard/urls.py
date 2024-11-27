245995e559ceeda645eaf61af2ab7c93be8ba243
Kiash254 <codewithkiash@gmail.com>
2024-11-27 10:29:01 +0300

    added migrations

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,10 @@
 # dashboard/urls.py
 from django.urls import path
-from .views import social_media_account_list, post_list
+from .views import social_media_account_list, social_media_account_detail, post_list, post_detail
 
 urlpatterns = [
     path('accounts/', social_media_account_list),
+    path('accounts/<int:pk>/', social_media_account_detail),
     path('posts/', post_list),
+    path('posts/<int:pk>/', post_detail),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -18,6 +18,26 @@ def social_media_account_list(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+@api_view(['GET', 'PUT', 'DELETE'])
+def social_media_account_detail(request, pk):
+    try:
+        account = SocialMediaAccount.objects.get(pk=pk)
+    except SocialMediaAccount.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = SocialMediaAccountSerializer(account)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = SocialMediaAccountSerializer(account, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        account.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 @api_view(['GET', 'POST'])
 def post_list(request):
     if request.method == 'GET':
@@ -30,3 +50,23 @@ def post_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def post_detail(request, pk):
+    try:
+        post = Post.objects.get(pk=pk)
+    except Post.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
