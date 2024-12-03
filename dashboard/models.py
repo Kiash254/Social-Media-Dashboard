f098c0e37cb57549beefbca89fd0335d182c4778
Kiash254 <codewithkiash@gmail.com>
2024-12-03 12:35:42 +0300

    added new data

--- dashboard/models.py
+++ dashboard/models.py
@@ -2,9 +2,10 @@
 from django.db import models
 
 class SocialMediaAccount(models.Model):
-    platform = models.CharField(max_length=50)
+    platform = models.CharField(max_length=100)
     username = models.CharField(max_length=100)
     access_token = models.CharField(max_length=255)
+    followers = models.IntegerField(default=0)  # New field
 
     def __str__(self):
         return self.username
@@ -13,6 +14,8 @@ class Post(models.Model):
     account = models.ForeignKey(SocialMediaAccount, on_delete=models.CASCADE)
     content = models.TextField()
     scheduled_time = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.content
